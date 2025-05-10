a6f418df4abc99931d8bde7670bd7f30df115071
krolik <krolik@yandex.ru>
2025-05-10 14:53:48 +0300

    fix

--- internal/infrastructure/socks5/connection.go
+++ internal/infrastructure/socks5/connection.go
@@ -10,9 +10,9 @@ import (
 
 type connection struct {
 	net.Conn
-	BytesRead        uint64
-	BytesWritten     uint64
-	UserId           uint32
+	bytesRead        uint64
+	bytesWritten     uint64
+	userId           uint32
 	statisticTracker *statistic.Tracker
 	logger           *slog.Logger
 }
@@ -20,11 +20,11 @@ type connection struct {
 func (c *connection) Close() error {
 	const op = "socks5.connection.Close"
 
-	if c.BytesRead > 0 || c.BytesWritten > 0 {
-		c.logger.Debug("Socks5 dial connection closing", "user", c.UserId, "in", c.BytesRead, "out", c.BytesWritten)
+	if c.bytesRead > 0 || c.bytesWritten > 0 {
+		c.logger.Debug("Socks5 dial connection closing", "user", c.userId, "in", c.bytesRead, "out", c.bytesWritten)
 
-		c.statisticTracker.Track(c.UserId, c.BytesRead, c.BytesWritten)
-		c.BytesRead, c.BytesWritten = 0, 0
+		c.statisticTracker.Track(c.userId, c.bytesRead, c.bytesWritten)
+		c.bytesRead, c.bytesWritten = 0, 0
 	}
 
 	if err := c.Conn.Close(); err != nil {
@@ -51,7 +51,7 @@ func (c *connection) Read(b []byte) (int, error) {
 	}
 
 	if n > 0 {
-		c.BytesRead += uint64(n)
+		c.bytesRead += uint64(n)
 	}
 
 	return n, nil
@@ -70,7 +70,7 @@ func (c *connection) Write(b []byte) (int, error) {
 	}
 
 	if n > 0 {
-		c.BytesWritten += uint64(n)
+		c.bytesWritten += uint64(n)
 	}
 
 	return n, nil
